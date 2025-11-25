c2c64644858b21b3200cabec674fad882d5fdee8
shia <66209618+ZenonTH-bot@users.noreply.github.com>
2025-11-25 08:55:19 +0700

    Add files via upload

---
+++ baitap/BT3/BT3-EX2.py
@@ -0,0 +1,14 @@
+try:
+    hours = float(input("Enter Hours: "))
+    rate = float(input("Enter Rate: "))
+    
+    if hours > 40:
+        regular_pay = 40 * rate
+        overtime_pay = (hours - 40) * rate * 1.5
+        pay = regular_pay + overtime_pay
+    else:
+        pay = hours * rate
+        
+    print("Pay:", pay)
+except ValueError:
+    print("Error, please enter numeric input")
